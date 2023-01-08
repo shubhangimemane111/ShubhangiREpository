96b0859a2228cea94a8f3e1cc1ff5efbb8d24036
lbalgude17 <lbbalgude17@gmail.com>
2023-01-08 22:45:39 +0530

    Basic steps
    Hi Team,
    I have Created Framework structure and go head clone it and start with your task.

---
+++ SimpleProject/src/mypackage/MyClass.java
@@ -0,0 +1,13 @@
+package mypackage;
+
+public class MyClass {
+
+	public static void main(String[]args) {
+		
+		
+		System.out.println("Added Dependancyes");
+		System.out.println("extent report");
+		System.out.println("log4j");
+		System.out.println("basic staff");
+	}
+}
